2e1710d8ec3810b72b7bfa9d4f75f32365313142
jumps <jumpsareop@gmail.com>
2023-06-21 13:14:23 +0300

    Better error handling (FIXED)

--- unlockpt.c
+++ unlockpt.c
@@ -1,14 +1,27 @@
-int puts(const char *);
+int puts(const char*);
 int grantpt(int);
 int unlockpt(int);
 char *ptsname(int);
+int perror(const char*);
+
+#define NULL (void*)0
+
+#define errif(cond,errmsg) if(cond){ perror(#errmsg); return 1;}
 
 int main(int argc, char *argv[]){
-	if(argc != 1) puts(
-		"unlockpt  Grant and unlock stdin slave PTY\n"
-		"USAGE: unlockpt <&UNGRATND_AND_LOCKED_PTY_FD\n"
-		"NOTE: This is useful after a `exec 3<>/dev/ptmx`"
-	); else
-		return (grantpt(0) || unlockpt(0)) ? 1 : (ptsname(0) ? 0 : 0);
-	return 1;
+	if(argc != 1){
+		puts(
+			"unlockpt  Grant and unlock stdin slave PTY\n"
+			"USAGE: unlockpt <&UNGRATND_AND_LOCKED_PTY_FD\n"
+			"NOTE: This is useful after a `exec 3<>/dev/ptmx`"
+		);
+		return 1;
+	}
+
+	errif(grantpt(0) == -1, grantpt)
+	errif(unlockpt(0) == -1, unlockpt)
+
+	char *f = ptsname(0);
+	errif(f == NULL, ptsname)
+	puts(ptsname(0));
 }
